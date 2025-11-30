f60ec2e084041ad7a1701972c20c970dbc6ce3cd
dqrta <dequarta@icloud.com>
2025-11-30 13:23:55 +0300

    Fixed some remarks

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,18 +1,29 @@
 import UIKit
 
+
 final class MovieQuizViewController: UIViewController {
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        let viewModel = getCurrentQuestionModelView()
-        show(quiz: viewModel)
-        super.viewDidLoad()
-    }
-    
+        
     private struct QuizQuestion {
         let image: String
         let text: String
         let correctAnswer: Bool
     }
+    
+    enum Strings {
+        static let correctAnswersText = "Вы ответили правильно на %d вопрос(ов) из %d."
+        static let roundOverText = "Раунд окончен"
+        static let retryText = "Сыграть еще раз!"
+    }
+    
+    private var correctAnswers: Int = 0
+    private var currentQuestionIndex: Int = 0
+    
+    @IBOutlet private weak var noButton: UIButton!
+    @IBOutlet private weak var yesButton: UIButton!
+    
+    @IBOutlet private weak var questionImageView: UIImageView!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var textLabel: UILabel!
     
     private let questions: [QuizQuestion] = [
         QuizQuestion(
@@ -67,16 +78,11 @@ final class MovieQuizViewController: UIViewController {
         )
     ]
     
-    
-    private var correctAnswers: Int = 0
-    private var currentQuestionIndex: Int = 0
-    
-    @IBOutlet private weak var noButton: UIButton!
-    @IBOutlet private weak var yesButton: UIButton!
-    
-    @IBOutlet private weak var questionImageView: UIImageView!
-    @IBOutlet private weak var counterLabel: UILabel!
-    @IBOutlet private weak var textLabel: UILabel!
+    override func viewDidLoad() {
+        let viewModel = getCurrentQuestionModelView()
+        show(quiz: viewModel)
+        super.viewDidLoad()
+    }
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel(
@@ -98,11 +104,15 @@ final class MovieQuizViewController: UIViewController {
     }
     
     private func show(quiz result: QuizResultsViewModel) {
-        let alert = UIAlertController(title: result.title,
-                                      message: result.text,
-                                      preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert
+        )
         
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             self.questionImageView.layer.borderColor = nil
@@ -112,15 +122,15 @@ final class MovieQuizViewController: UIViewController {
         }
         
         alert.addAction(action)
-        
         self.present(alert, animated: true, completion: nil)
     }
     
     private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
         
-        if isCorrect { correctAnswers += 1 }
-        
-        questionImageView.layer.borderWidth = 1
+        questionImageView.layer.borderWidth = 8
         questionImageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         
         self.switchEnableButtons()
@@ -138,12 +148,10 @@ final class MovieQuizViewController: UIViewController {
     
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questions.count - 1 {
-            let text = "Вы ответили правильно на \(correctAnswers) вопрос(ов) из \(questions.count)."
-            
             let viewModel = QuizResultsViewModel(
-                title: "Раунд окончен",
-                text: text,
-                buttonText: "Сыграть еще раз!")
+                title: Strings.roundOverText,
+                text: String(format: Strings.correctAnswersText, correctAnswers, questions.count),
+                buttonText: Strings.retryText)
             
             show(quiz: viewModel)
         } else {
